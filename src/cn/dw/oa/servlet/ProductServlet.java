998e88204ec0bd24376ef302b56aad2dcd0d455b
Administrator <Administrator@DESKTOP-D7Q7Q9F>
2018-07-18 17:02:21 +0800

    完成删除操作,如果多个浏览器并发访问删除之后的查询会出现线程安全问题(此问题下一个版本会解决)

--- src/cn/dw/oa/servlet/ProductServlet.java
+++ src/cn/dw/oa/servlet/ProductServlet.java
@@ -26,46 +26,62 @@ public class ProductServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	private ProductServiceImpl productService=new ProductServiceImpl();
+	// web中java支持并发访问,如果是单例模式(一个对象)都需要考虑线程安全问题
+	private String keyword = null;
 
 	public ProductServlet() {
 		super();
+		System.out.println("ProductServlet()...");
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// 1: 获取前端的数据 (通过前台的表单元素的名称来获取数据)
-		String keyword = request.getParameter("keyword");
-		// 2: 调用Service业务逻辑(Servlet自身不负责任何功能模块的实现)
-		List<Product> proList = productService.queryByName(keyword);
-		request.setAttribute("proList", proList);
-		// 能够存放在request数据就不应该存放在session中
-//		session.setAttribute("proList",proList);
-		// 3: 跳转到下一个页面(服务器给客户端的响应)
-//		response.sendRedirect("/web/query.jsp");
-		// java web中有两种跳转: 
-		  // 重定向: Redirect(先访问servlet,然后客户端在访问query.jsp)
-		  // 转发: forward默认已经添加工程名(只能转发到系统内部资源)
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/query.jsp");
-		// 如果 servlet与jsp页面存在数据的共享则必须使用转发而非重定向
-		dispatcher.forward(request, response);
+		this.doPost(request, response);
 	}
 
 	// doPost有两个参数: request：用来封装客户端到服务器端的请求数据
 	// response: 封装服务器端返回给客户端的数据
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		System.out.println("request:" + request);
-		System.out.println("doPost..............");
-		// 1: 获取前端的数据 (通过前台的表单元素的名称来获取数据)
-		Product product = new Product();
-		// ctrl + 2 shift+alt+A ctrl + shift + F
-		product.setName(request.getParameter("name"));
-		product.setRemark(request.getParameter("remark"));
-		product.setPrice(Double.parseDouble(request.getParameter("price")));
-		// 2: 调用Service业务逻辑(Servlet自身不负责任何功能模块的实现)
-		productService.save(product);
-		// 3: 跳转到下一个页面(服务器给客户端的响应)
-		response.sendRedirect("/web/query.jsp");
+		// 1: 获取type的值,根据值来判断具体的操作
+		String type = request.getParameter("type");
+		if(type.equals("save")) {
+			// 1: 获取前端的数据 (通过前台的表单元素的名称来获取数据)
+			Product product = new Product();
+			// ctrl + 2 shift+alt+A ctrl + shift + F
+			product.setName(request.getParameter("name"));
+			product.setRemark(request.getParameter("remark"));
+			product.setPrice(Double.parseDouble(request.getParameter("price")));
+			// 2: 调用Service业务逻辑(Servlet自身不负责任何功能模块的实现)
+			productService.save(product);
+			// 3: 跳转到下一个页面(服务器给客户端的响应)
+			response.sendRedirect("/web/query.jsp");
+		}else if(type.equals("query")) {
+			// 1: 获取前端的数据 (通过前台的表单元素的名称来获取数据)
+			keyword = request.getParameter("keyword");
+			// 2: 调用Service业务逻辑(Servlet自身不负责任何功能模块的实现)
+			List<Product> proList = productService.queryByName(keyword);
+			request.setAttribute("proList", proList);
+			// java web中有两种跳转: 
+			  // 重定向: Redirect(先访问servlet,然后客户端在访问query.jsp)
+			  // 转发: forward默认已经添加工程名(只能转发到系统内部资源)
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/query.jsp");
+			// 如果 servlet与jsp页面存在数据的共享则必须使用转发而非重定向
+			dispatcher.forward(request, response);
+		}else if(type.equals("delete")) {
+			String id = request.getParameter("id");
+			productService.delete(Integer.parseInt(id));
+			// 获取之前的查询关键字,重新查询
+			productService.queryByName(keyword);
+			List<Product> proList = productService.queryByName(keyword);
+			request.setAttribute("proList", proList);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/query.jsp");
+			dispatcher.forward(request, response);
+		}
 	}
 
 }
+
+
+
+
